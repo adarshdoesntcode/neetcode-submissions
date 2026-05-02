1875f018ab5759212977f6c85ba822fb1849fcfe
Adarsh <51852793+adarshdoesntcode@users.noreply.github.com>
2026-05-02 23:49:24 +0545

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        const alphanumeric = s.replace(/[^A-Za-z0-9]/g,"").toLowerCase()
+        const l = alphanumeric.length
+
+        if(l > 0){
+            for(let i = 0 ; (l - 1 - i) - i !== -1 && (l - 1 - i) - i !== 0 ; i++){
+                if(!(alphanumeric[i] === alphanumeric[l - 1 - i])){
+                    return false
+                }
+            }
+        }
+
+        return true
+    }
+}

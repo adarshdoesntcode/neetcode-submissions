b78a0a1f5dde5840664e9c6e21e0995fe64e6f0a
Adarsh <51852793+adarshdoesntcode@users.noreply.github.com>
2026-05-02 18:22:28 +0545

    Add: longest-consecutive-sequence - submission-8

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-8.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        const sorted = Array.from(new Set(nums.sort((a,b)=> a-b)));
+        let initialStreak = sorted.length > 0 ? 1 : 0;
+        let currentMax = initialStreak
+        let currentStreak  = initialStreak
+
+        if(sorted.length > 1){
+
+        for(let i = 0; i < sorted.length - 1; i++){
+            if(sorted[i+1] === sorted[i] + 1){
+                currentStreak = currentStreak+1
+                if (currentStreak >= currentMax){
+                    currentMax = currentStreak
+                }
+            }else{
+                currentStreak  = initialStreak
+            }
+        }
+        }else{
+            currentMax = initialStreak
+        }
+
+        return currentMax
+    }
+}
